909bf796388056580c72a47cd7329bcb0ea8f74f
Cameron <cameroncronheimer@gmail.com>
2021-08-22 19:07:03 -0400

    Initial Commit

---
+++ CoinHawk/CoinHawkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinHawkApp.swift
+//  CoinHawk
+//
+//  Created by Cameron Cronheimer on 2021-08-22.
+//
+
+import SwiftUI
+
+@main
+struct CoinHawkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinHawk/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoinHawk
+//
+//  Created by Cameron Cronheimer on 2021-08-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
